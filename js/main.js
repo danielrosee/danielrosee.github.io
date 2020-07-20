be4cc97aa8b82350190afa1fb8f6d08687fe4470
danielrosee <danielrosebest@gmail.com>
2020-07-20 23:08:16 +0200

    init commit

---
+++ js/main.js
@@ -0,0 +1,32 @@
+$(function () {
+	$(window).scroll(function() {
+	    $('.skills').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInUp");
+	        }
+	    });
+	});
+	$(window).scroll(function() {
+	    $('.section-title').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInLeft");
+	        }
+	    });
+	});
+	$(window).scroll(function() {
+	    $('#mail .section-title').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("backInDown");
+	        }
+	    });
+	});
+})
